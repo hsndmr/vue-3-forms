a0b91afb5b2b81ec9314b9e5a33f5a28d208ef9c
Hasan Demir <iletisim@hasandemir.org>
2022-04-10 05:45:36 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import BaseInput from "./components/BaseInput.vue";
+const input = ref("");
+</script>
+
+<template>
+  <form>
+    <BaseInput label="Input" v-model="input" />
+    <div>{{ input }}</div>
+  </form>
+</template>
+
+<style></style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  label?: string;
+  modelValue?: string;
+}>();
+</script>
+
+<template>
+  <label>{{ label }}</label>
+  <br />
+  <input
+    :placeholder="(label as any)"
+    class="form-input"
+    :value="modelValue"
+    @input="$emit('update:modelValue', ($event as any).target.value)"
+  />
+</template>
