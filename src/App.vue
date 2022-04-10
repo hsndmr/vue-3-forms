8c36a72b2ebdeafd81a77d93ce37667901303761
Hasan Demir <iletisim@hasandemir.org>
2022-04-10 06:11:40 +0300

    Add BaseSelect component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,28 @@
 import { ref } from "vue";
 
 import BaseInput from "./components/BaseInput.vue";
+import BaseSelect from "./components/BaseSelect.vue";
 const form = ref({
   input: "",
+  select: "a",
 });
+const options = ref(["a", "b"]);
 </script>
 
 <template>
   <form>
+    <!-- Base Input -->
     <BaseInput type="text" label="Input" v-model="form.input" />
     <div>{{ form.input }}</div>
+    <br />
+    <!-- Base Select -->
+    <BaseSelect
+      label="Select"
+      v-model="form.select"
+      :options="(options as any)"
+    />
+    <div>{{ form.select }}</div>
+    <br />
   </form>
 </template>
 
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+  label?: string;
+  modelValue?: string;
+  options: string[];
+}>();
+</script>
+
+<template>
+  <label v-if="label">{{ label }}</label>
+  <select
+    :value="modelValue"
+    class="form-input"
+    v-bind="{
+      // @ts-ignore
+      ...$attrs,
+      onChange: ($event) => {
+        $emit('update:modelValue', ($event as any).target.value);
+      },
+    }"
+  >
+    <option
+      :key="(option as any)"
+      v-for="option in options"
+      :value="option"
+      :selected="option === modelValue"
+    >
+      {{ option }}
+    </option>
+  </select>
+</template>
